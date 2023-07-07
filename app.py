9c8ad81618872d9161812ccf64a476284e8ec204
Harshit Pokhriyal <76612251+geeker-com@users.noreply.github.com>
2023-07-07 20:23:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pickle
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+port_stem = PorterStemmer()
+vectorization = TfidfVectorizer()
+nltk.download('stopwords')
+vector_form = pickle.load(open('vect.pkl', 'rb'))
+load_model = pickle.load(open('fake_news.pkl', 'rb'))
+
+
+def stemming(content):
+    stemmed_content = re.sub('[^a-zA-Z]',' ',content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
+def fake_news(news):
+    news=stemming(news)
+    input_data=[news]
+    vector_form1=vector_form.transform(input_data)
+    prediction = load_model.predict(vector_form1)
+    return prediction
+
+
+
+if __name__ == '__main__':
+    st.title('Fake News Classification app ')
+    st.subheader("Input the News content below")
+    sentence = st.text_area("Enter your news content here", "",height=200)
+    predict_btt = st.button("predict")
+    if predict_btt:
+        prediction_class=fake_news(sentence)
+        print(prediction_class)
+        if prediction_class == [0]:
+            st.success('Reliable')
+        if prediction_class == [1]:
+            st.warning('Unreliable')
